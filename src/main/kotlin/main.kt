a9cab72f364c372ece19b9ed2d1cfe4966fbb7f2
YuSidorin <xepo@mail.ru>
2022-06-22 23:56:07 +0300

    add some new

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,36 @@
 fun main(){
-        println("Hello Kotlin")
+// first one
+    val fee = calculateFee(100_000_000)
+
+// second one
+    val likes: Int = 5
+    getLikesWord(likes)
+}
+
+// third one
+
+private fun calculateFee(amount: Int): Int {
+    val standartFee = 3500
+    var fee: Int = amount / 100 * 75
+    if (fee < standartFee) {
+        fee = standartFee
     }
+    return fee
+}
+
+private fun getLikesWord(likes: Int): String {
+    val firstCase = " людей"
+    val secondCase = " человека"
+    val thirdCase = " человек"
+    var string = ""
+    if (likes === 2 || likes === 3 || likes === 4){
+        string = secondCase
+    } else if (likes === 1){
+        string = thirdCase
+    } else {
+        string = firstCase
+    }
+    return string
+
+
+}
